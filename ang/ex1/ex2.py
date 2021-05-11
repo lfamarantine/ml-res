53053804fd538e815d2c072e2b4adbc6ebee3938
dpopadic <dario.popadic@yahoo.com>
2021-05-11 22:04:23 +0100

    ex1 modifications

--- ang/ex1/ex2.py
+++
@@ -1,8 +0,0 @@
-
-import numpy as np
-import matplotlib.pyplot as plt
-from matplotlib import cm
-from mpl_toolkits.mplot3d import Axes3D
-import os, sys
-sys.path.append(os.getcwd() + os.path.dirname('/ang/ex1/'))
-from helpers import warmUpExercise, computeCost, gradientDescent, plotData
