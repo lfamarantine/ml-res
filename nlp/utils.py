625ca2477f241ce6f7e4c15f9b2299b91c68c166
dpopadic <dario.popadic@yahoo.com>
2021-11-10 17:00:30 +0000

    nlp stuff

--- nlp/utils.py
+++ nlp/utils.py
@@ -44,17 +44,21 @@ def process_tweet(tweet):
 
 def build_freqs(tweets, ys):
     """Build frequencies.
-    Params:
-    -------
+    Input:
         tweets: a list of tweets
         ys: an m x 1 array with the sentiment label of each tweet
             (either 0 or 1)
-    Returns:
-    --------
+    Output:
         freqs: a dictionary mapping each (word, sentiment) pair to its
         frequency
     """
+    # Convert np array to list since zip needs an iterable.
+    # The squeeze is necessary or the list ends up with one element.
+    # Also note that this is just a NOP if ys is already a list.
     yslist = np.squeeze(ys).tolist()
+
+    # Start with an empty dictionary and populate it by looping over all tweets
+    # and over all processed words in each tweet.
     freqs = {}
     for y, tweet in zip(yslist, tweets):
         for word in process_tweet(tweet):
@@ -63,5 +67,4 @@ def build_freqs(tweets, ys):
                 freqs[pair] += 1
             else:
                 freqs[pair] = 1
-
     return freqs
